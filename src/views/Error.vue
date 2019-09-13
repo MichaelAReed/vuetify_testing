8f130cd08d14b51e3421723160ed665c9329e891
Mike Reed <michael.reed@live.com.au>
2019-09-13 21:25:11 +0800

    just added an error page and getting familiar with everything again after a break.

---
+++ src/views/Error.vue
@@ -0,0 +1,10 @@
+<template>
+	<div>
+		<h1>This is an error page</h1>
+		<h1>Error page</h1>
+		<div>
+			Sorry - something went wrong.
+			<router-link to="/">Go back</router-link>
+		</div>
+	</div>
+</template>
